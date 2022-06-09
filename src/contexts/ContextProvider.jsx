963a4bb7443d68b9d928e30516934b5fc6e6aeba
Vent2 <ventura.c.gerald@gmail.com>
2022-06-09 03:03:18 -0400

    Context api

---
+++ src/contexts/ContextProvider.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const StateContext = createContext();
+
+const initalState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false,
+}
+
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setactiveMenu] = useState(true) ;
+
+    return (
+        <StateContext.Provider value={{activeMenu, setactiveMenu}}>
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
