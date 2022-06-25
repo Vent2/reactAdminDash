4d9c153f12c37c87ab45ea67c44296d15b42f00b
Vent2 <ventura.c.gerald@gmail.com>
2022-06-25 03:43:26 -0400

    Done

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,30 +1,3 @@
-// import React from 'react'
-// import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
-
-  
-
-// const SparkLine = ({ id, height, width, color, data, type, currentColor}) => {
-//   return (
-//     <SparklineComponent
-//       id={id}
-//       height={height}
-//       width={width}
-//       lineWidth={1}
-//       valueType='Numeric'
-//       fill={color}
-//       border={{color: currentColor, width: 2 }}
-//       dataSource={data}
-//       xName='x'
-//       yName='y'
-//       type={type}
-//     >
-//       <Inject services={[SparklineTooltip]} />
-//     </SparklineComponent>
-//   )
-// }
-
-// export default SparkLine
-
 import React from 'react';
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
@@ -43,6 +16,7 @@ class SparkLine extends React.PureComponent {
         border={{ color: currentColor, width: 2 }}
         tooltipSettings={{
           visible: true,
+          // eslint-disable-next-line no-template-curly-in-string
           format: '${x} : data ${yval}',
           trackLineSettings: {
             visible: true,
--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
 import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban';
-import { kanbanData, kanbanGrid } from '../data/dummy'
-import { Header } from '../components'
 
+import { kanbanData, kanbanGrid } from '../data/dummy';
+import { Header } from '../components';
 
-
-const Kanban = () => {
-  return (
-    <div className='m-2 md:m-10 mt-24 p-2 md:p-10m bg-white rounded-3xl'>
-      <Header title='Kanban' category="App" />
-      <KanbanComponent
-        id="kanban"
-        dataSource={kanbanData}
-        cardSettings={{ 
-          contentField: 'Summary',
-          headerField: 'Id'
-      }}
+const Kanban = () => (
+  <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="App" title="Kanban" />
+    <KanbanComponent
+      id="kanban"
       keyField="Status"
-      >
-        <ColumnsDirective>
-          {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
-        </ColumnsDirective>
-      </KanbanComponent>
-    </div>
-  )
-}
+      dataSource={kanbanData}
+      cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
+    >
+      <ColumnsDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+      </ColumnsDirective>
+    </KanbanComponent>
+  </div>
+);
 
-export default Kanban
+export default Kanban;
